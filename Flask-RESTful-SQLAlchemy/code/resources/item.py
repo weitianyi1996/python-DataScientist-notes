476624c94d673364f9b33e054abe4bdea5d39e8b
weitianyi1996 <weitianyius1996@163.com>
2020-08-23 16:09:02 -0400

    use SQLAlchemy to replace SQLite in ItemList(query all)

--- Flask-RESTful-SQLAlchemy/code/resources/item.py
+++ Flask-RESTful-SQLAlchemy/code/resources/item.py
@@ -74,20 +74,20 @@ class Item(Resource):
 
 class ItemList(Resource):
     def get(self):
-        connection = sqlite3.connect("data.db")
-        cursor = connection.cursor()
+        # connection = sqlite3.connect("data.db")
+        # cursor = connection.cursor()
+        #
+        # query = "SELECT * FROM items"
+        # result = cursor.execute(query)
+        #
+        # items = []
+        # for row in result:
+        #     items.append({
+        #         "name": row[0],
+        #         "price": row[1]
+        #     })
+        #
+        # # connection.commit()  # need to commit! when editing datatable
+        # connection.close()
 
-        query = "SELECT * FROM items"
-        result = cursor.execute(query)
-
-        items = []
-        for row in result:
-            items.append({
-                "name": row[0],
-                "price": row[1]
-            })
-
-        # connection.commit()  # need to commit! when editing datatable
-        connection.close()
-
-        return {"items": items}
+        return {"items": [item.json() for item in ItemModel.query.all()]}  # item already in class instance object
